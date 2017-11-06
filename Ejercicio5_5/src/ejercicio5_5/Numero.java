16708379cff5087e23b134fd70930140e12f3863
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-11-06 09:18:02 +0100

    Clase completa

---
+++ Ejercicio5_5/src/ejercicio5_5/Numero.java
@@ -0,0 +1,14 @@
+package ejercicio5_5;
+/*
+ * @author Arturo
+ */
+public class Numero {
+    private int x;
+    
+    public Numero(int n){
+        x=n;
+    }
+    public int getNumero(){
+        return x;
+    }
+}
